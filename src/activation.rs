0d06f038f44928a79ccf90af7d6ea0a81b3cd618
mmasque <marcel.masques@gmail.com>
2022-08-27 23:48:06 +0200

    added some scaffolding for backprop

--- src/activation.rs
+++ src/activation.rs
@@ -1,18 +1,29 @@
 pub trait Activation {
-    fn activate(input: &Vec<f32>) -> Vec<f32>;
+    fn activate(&self, input: &Vec<f32>) -> Vec<f32>;
+}
+
+pub trait Derivative {
+    fn derivative(&self, input: &Vec<f32>) -> Vec<f32>;
 }
 
 pub struct ReLU;
 pub struct Softmax;
 
 impl Activation for ReLU {
-    fn activate(input: &Vec<f32>) -> Vec<f32> {
+    fn activate(&self, input: &Vec<f32>) -> Vec<f32> {
         vec![input.iter().map(|x| if x < &0.0 { 0.0 } else { *x }).sum()]
     }
 }
+
 impl Activation for Softmax {
-    fn activate(input: &Vec<f32>) -> Vec<f32> {
+    fn activate(&self, input: &Vec<f32>) -> Vec<f32> {
         let exp_sum: f32 = input.iter().map(|x| x.exp()).sum();
         input.iter().map(|x| x.exp() / exp_sum).collect()
+    }
+}
+
+impl Derivative for ReLU {
+    fn derivative(&self, input: &Vec<f32>) -> Vec<f32> {
+        unimplemented!()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod activation;
 use crate::activation::{Activation, ReLU, Softmax};
+use ndarray::{Array1, Array2, ShapeError};
 fn main() {
     let test = vec![-1.0, 2.0, 3.0, 4.0];
-    let val = ReLU::activate(&test);
+    let val = ReLU {}.activate(&test);
     println!("{:?}", val);
-    println!("{:?}", Softmax::activate(&test));
+    println!("{:?}", Softmax {}.activate(&test));
+    let t = ReLU {};
 }
 
 // the first goal is to build a simple neural network.
@@ -15,3 +17,41 @@ fn main() {
 // will need to implement forward propagation
 // will need to implement backpropagation
 // the dataset will be MINST, from here: https://deepai.org/dataset/mnist
+
+// this activation layout is already a bit annoying:
+pub struct Layer<T, F: Activation, Derivative> {
+    pub Weights: Array2<T>,
+    pub Biases: Array1<T>,
+    pub Activation: F,
+}
+
+// what do layers need?
+// inputs to each node
+// outputs from each node (or none)
+// weights
+// biases
+// activation
+
+// want: to note the num_nodes, num_inputs as requirements for the input dimensions of forward
+impl<F: Activation, Derivative> Layer<f32, F> {
+    pub fn new(activation: F, num_nodes: usize, num_inputs: usize) -> Self {
+        Self {
+            Weights: Array2::<f32>::zeros((num_nodes, num_inputs)),
+            Biases: Array1::<f32>::zeros(num_nodes),
+            Activation: activation,
+        }
+    }
+    // forward pass
+    pub fn forward(&self, inputs: Array1<f32>) -> Array1<f32> {
+        // could maybe use a Box and std [[]] since then we can provide dimensions and avoid
+        // the possible panic from Weights.dot
+        self.Weights.dot(&inputs) + &self.Biases
+    }
+
+    pub fn backpropagation(&self, inputs: Array1<f32>, dC_dq: Array1<f32>) -> Array1<f32> {
+        unimplemented!();
+        // we want, for loss C, dC/dw for each weight w, and dC/db for each bias b.
+        // chain rule: dq/dw * dC/dq = dc/dw, for q any previous (right to left) ops.
+        let i_d = self.Activation.derivative(inputs);
+    }
+}
